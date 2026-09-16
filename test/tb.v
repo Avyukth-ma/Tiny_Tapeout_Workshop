129fbd8f86741d1aaacd2bc2a3d920aac24b8ef1
Avyukth M A <ma.avyukth@gmail.com>
2026-09-16 12:09:16 +0530

    Update testbench for TinyRV32

--- test/tb.v
+++ test/tb.v
@@ -3,16 +3,14 @@
 
 module tb ();
 
-  // Clock, reset and enable
-  reg clk;
-  reg rst_n;
-  reg ena;
+  // Tiny Tapeout interface
+  reg        clk;
+  reg        rst_n;
+  reg        ena;
 
-  // Tiny Tapeout inputs
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
+  reg  [7:0] ui_in;
+  reg  [7:0] uio_in;
 
-  // Tiny Tapeout outputs
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
@@ -30,11 +28,10 @@ module tb ();
     #1;
   end
 
-  // Instantiate 4-bit ALU
-  tt_um_avyukth_alu user_project (
+  // Instantiate TinyRV32
+  tt_um_avyukth_tinyrv32 user_project (
 
 `ifdef GL_TEST
-      // Power ports for gate-level simulation
       .VPWR(VPWR),
       .VGND(VGND),
 `endif
